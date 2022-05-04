13212bb6b3a8e0d68585bd93cca94a3e1ab09da5
SKY <onthsky@gmail.com>
2022-05-04 16:16:56 +0300

    applying change to add database and receive data from internet

--- app/src/main/java/com/sky/recapfinalproject/model/Food.kt
+++ app/src/main/java/com/sky/recapfinalproject/model/Food.kt
@@ -5,6 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+// Model
 @Entity(tableName = "tblFood")
 data class Food(
     @ColumnInfo(name="name")
