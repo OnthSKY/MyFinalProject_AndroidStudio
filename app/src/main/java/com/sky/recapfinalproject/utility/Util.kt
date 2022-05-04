9d2f6c25e6289bbe04577df34ff62b8fb2264cc8
SKY <onthsky@gmail.com>
2022-05-04 16:30:29 +0300

    adding method extensions into views with applying Glide to download image from internet

--- app/src/main/java/com/sky/recapfinalproject/utility/Util.kt
+++ app/src/main/java/com/sky/recapfinalproject/utility/Util.kt
@@ -7,11 +7,12 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.sky.recapfinalproject.R
 
+// image will be downloaded from internet into imageView with Glide
 fun ImageView.downloadFromInternet(url: String?, placeHolder:CircularProgressDrawable){
     val options = RequestOptions.placeholderOf(placeHolder).error(R.mipmap.ic_launcher_round)
     Glide.with(context).setDefaultRequestOptions(options).load(url).into(this)
 }
-
+// while data were loading, circular refresh  will be displayed in imageView
 fun createPlaceHolder(context: Context) : CircularProgressDrawable{
     return CircularProgressDrawable(context).apply {
         strokeWidth=8F
